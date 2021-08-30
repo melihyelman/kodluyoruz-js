cb0c394dca5ed761b7bad0a2ccedb9e74623a33d
melihyelman <myelman17@gmail.com>
2021-08-30 16:08:44 +0300

    add navbar and auth pages

--- e-commerce-app/client/src/App.js
+++ e-commerce-app/client/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Signup from './pages/Auth/Signup';
+import Signin from './pages/Auth/Signin';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+
+        <div id='content'>
+          <Switch>
+            <Route path='/' exact component={Home}></Route>
+            <Route path='/signin' component={Signin}></Route>
+            <Route path='/signup' component={Signup}></Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
 }
 
 export default App;
--- e-commerce-app/client/src/index.js
+++ e-commerce-app/client/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './reset.css';
 import App from './App';
+import { ChakraProvider } from '@chakra-ui/react';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
