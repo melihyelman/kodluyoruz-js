39a86c4fdc603e5c41f14e82f8fd32e564ef46a5
melihyelman <myelman17@gmail.com>
2021-09-03 22:09:33 +0300

    add toggle completed

--- todo-app-redux/src/components/TodoList.js
+++ todo-app-redux/src/components/TodoList.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { toggle } from '../redux/todos/todosSlice';
 
 import { useSelector } from 'react-redux';
 
 function TodoList() {
   const items = useSelector((state) => state.todos.items);
-  console.log(items);
+  const dispatch = useDispatch();
 
   return (
     <ul className='todo-list'>
       {items.map((item) => (
         <li className={item.completed ? 'completed' : ''} key={item.id}>
           <div className='view'>
-            <input className='toggle' type='checkbox' />
+            <input
+              className='toggle'
+              type='checkbox'
+              checked={item.completed}
+              onChange={() => dispatch(toggle({ id: item.id }))}
+            />
             <label>{item.title}</label>
             <button className='destroy'></button>
           </div>
