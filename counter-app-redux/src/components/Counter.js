e17a9d42f4325810e154d2ca48ec12d919920c92
melihyelman <myelman17@gmail.com>
2021-09-03 17:29:48 +0300

    add increment and decrement

--- counter-app-redux/src/components/Counter.js
+++ counter-app-redux/src/components/Counter.js
@@ -1,9 +1,34 @@
-import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  increment,
+  incrementByAmount,
+  decrement,
+} from '../redux/counter/counterSlice';
 
 function Counter() {
+  const [amount, setAmount] = useState(1);
   const countValue = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
-  return <h1>{countValue}</h1>;
+  return (
+    <div>
+      <h1>{countValue}</h1>
+
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <br />
+      <br />
+      <input
+        type='number'
+        value={amount}
+        onChange={({ target }) => setAmount(target.value)}
+      />
+      <button onClick={() => dispatch(incrementByAmount(amount))}>
+        Increment by amount
+      </button>
+    </div>
+  );
 }
 
 export default Counter;
