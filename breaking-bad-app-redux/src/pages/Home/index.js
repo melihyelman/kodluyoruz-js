09311f45ab704dcb148ec16afd9b5d8ce00ee8f9
melihyelman <myelman17@gmail.com>
2021-09-05 02:05:26 +0300

    list characters

--- breaking-bad-app-redux/src/pages/Home/index.js
+++ breaking-bad-app-redux/src/pages/Home/index.js
@@ -1,10 +1,17 @@
 import { useEffect } from 'react';
-
+import Masonry from 'react-masonry-css';
+import './styles.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCharacters } from '../../redux/charactersSlice';
+import Loading from '../../components/Loading';
+import Error from '../../components/Error';
 
 function Home() {
-  const data = useSelector((state) => state.characters);
+  const characters = useSelector((state) => state.characters.items);
+  const isLoading = useSelector((state) => state.characters.isLoading);
+  const nextPage = useSelector((state) => state.characters.page);
+  const hasNextPage = useSelector((state) => state.characters.hasNextPage);
+  const error = useSelector((state) => state.characters.error);
 
   const dispatch = useDispatch();
 
@@ -12,7 +19,37 @@ function Home() {
     dispatch(fetchCharacters());
   }, [dispatch]);
 
-  return <div>Home</div>;
+  if (error) return <Error message={error} />;
+
+  return (
+    <div>
+      <Masonry
+        breakpointCols={4}
+        className='my-masonry-grid'
+        columnClassName='my-masonry-grid_column'
+      >
+        {characters.map((character) => (
+          <div key={character.char_id}>
+            <img
+              src={character.img}
+              alt={character.name}
+              className='character'
+            />
+            <div className='charName'>{character.name}</div>
+          </div>
+        ))}
+      </Masonry>
+      <div style={{ padding: '20px 0 40px 0', textAlign: 'center' }}>
+        {isLoading && <Loading />}
+        {hasNextPage && !isLoading && (
+          <button onClick={() => dispatch(fetchCharacters(nextPage))}>
+            Load more ({nextPage})
+          </button>
+        )}
+        {!hasNextPage && !isLoading && <div>There is nothing to be show</div>}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- breaking-bad-app-redux/src/redux/charactersSlice.js
+++ breaking-bad-app-redux/src/redux/charactersSlice.js
@@ -1,11 +1,15 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+const charLimit = 12;
+
 export const fetchCharacters = createAsyncThunk(
   'characters/getCharacters',
-  async () => {
+  async (page) => {
     const res = await axios(
-      `${process.env.REACT_APP_BASE_ENDPOINT}/characters?limit=10`
+      `${
+        process.env.REACT_APP_BASE_ENDPOINT
+      }/characters?limit=${charLimit}&offset=${page * charLimit}`
     );
 
     return res.data;
@@ -16,11 +20,25 @@ export const charactersSlice = createSlice({
   name: 'characters',
   initialState: {
     items: [],
+    isLoading: false,
+    page: 0,
+    hasNextPage: true,
   },
   reducers: {},
   extraReducers: {
+    [fetchCharacters.pending]: (state, action) => {
+      state.isLoading = true;
+    },
+    [fetchCharacters.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.error.message;
+    },
     [fetchCharacters.fulfilled]: (state, action) => {
-      console.log(action.payload);
+      if (action.payload.length < 12) state.hasNextPage = false;
+
+      state.items = [...state.items, ...action.payload];
+      state.isLoading = false;
+      state.page += 1;
     },
   },
 });
